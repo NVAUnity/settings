99e4f0ed132681d4ec9026900825b470aef3d00a
Nikita <108986997+NVAUnity@users.noreply.github.com>
2022-12-07 10:23:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const outputLine = document.querySelector('.output__line');
+const outputLine2 = document.querySelector('.output__line--2');
 outputLine.innerHTML = document.documentElement.clientWidth;
+outputLine2.innerHTML = document.documentElement.clientHeight;
